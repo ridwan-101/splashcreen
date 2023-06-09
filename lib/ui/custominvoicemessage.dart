ec9a0e780fa565ce52d6c77626592200c4e50010
ridwan-101 <abdulazeezridwan004@gmail.com>
2023-06-09 09:53:41 +0100

    onboarding screen for new users

--- lib/ui/custominvoicemessage.dart
+++ lib/ui/custominvoicemessage.dart
@@ -17,61 +17,72 @@ class _MessagePageState extends State<MessagePage> {
           "Create Customed Invoice",
           style: TextStyle(fontSize: 19.0, fontWeight: FontWeight.bold),
         ),
-        // centerTitle: true,
       ),
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Image(
-              image: AssetImage("assets/images/phone1.png"),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              "Design and personalize your invoices to reflect your brand's identity. Add your company logo and tailor the invoice fields to meet your specific business needs.",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.0),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            SingleChildScrollView(
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const FinalMessage()),
-                  );
-                  ;
-                },
-                child: Container(
-                  height: MediaQuery.of(context).size.height *
-                      0.06, // 20% of the screen height
-                  width: MediaQuery.of(context).size.width *
-                      0.9, // 80% of the screen width
-                  decoration: BoxDecoration(
-                    color: const Color(0xFF01497C),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-
-                  child: const Center(
-                    child: Text(
-                      "Next",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0,
-                        color: Colors.white,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/onthegobg.png"),
+            fit: BoxFit.cover,
+          ),
+          color:
+              Color(0xFF01497C), // Replace with your desired background color
+        ),
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Image(
+                image: AssetImage("assets/images/phone1.png"),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "Design and personalize your invoices to reflect your brand's identity. Add your company logo and tailor the invoice fields to meet your specific business needs.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15.0,
+                    color: Colors.white),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              SingleChildScrollView(
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const FinalMessage(),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    height: MediaQuery.of(context).size.height *
+                        0.06, // 20% of the screen height
+                    width: MediaQuery.of(context).size.width *
+                        0.9, // 80% of the screen width
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "Next",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          color: Color(0xFF01497C),
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/ui/welcomemessage.dart
+++ lib/ui/welcomemessage.dart
@@ -17,60 +17,70 @@ class _WelcomePageState extends State<WelcomePage> {
           "Welcome",
           style: TextStyle(fontSize: 19.0, fontWeight: FontWeight.bold),
         ),
-        // centerTitle: true,
       ),
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Image(
-              image: AssetImage("assets/images/person1.png"),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              " 'Simplify your invoicing process with OnDgo,\nthe ultimate invoice generating app designed\nto save you time and streamline your business.',",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.0),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            SingleChildScrollView(
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const MessagePage()),
-                  );
-                },
-                child: Container(
-                  height: MediaQuery.of(context).size.height *
-                      0.06, // 20% of the screen height
-                  width: MediaQuery.of(context).size.width *
-                      0.9, // 80% of the screen width
-                  decoration: BoxDecoration(
-                    color: const Color(0xFF01497C),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-
-                  child: const Center(
-                    child: Text(
-                      "Next",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0,
-                        color: Colors.white,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/onthegobg.png"),
+            fit: BoxFit.cover,
+          ),
+          color: Color(0xFF01497C),
+        ),
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Image(
+                image: AssetImage("assets/images/person1.png"),
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                " 'Simplify your invoicing process with OnDgo,\nthe ultimate invoice generating app designed\nto save you time and streamline your business.',",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15.0,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              SingleChildScrollView(
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const MessagePage()),
+                    );
+                  },
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.06,
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "Next",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          color: Color(0xFF01497C),
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
