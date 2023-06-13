aad0529327bdbb4b29d17df9503efd212fbc2aa6
ridwan-101 <abdulazeezridwan004@gmail.com>
2023-06-13 12:47:25 +0100

    information about ui update for onDgo

--- lib/ui/login_signupmessage.dart
+++ lib/ui/login_signupmessage.dart
@@ -14,9 +14,99 @@ class _SignupPageState extends State<SignupPage> {
       body: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
-              image: AssetImage("assets/images/onthegobg.png"),
-              fit: BoxFit.none),
+            image: AssetImage("assets/images/onthegobg.png"),
+            fit: BoxFit.none,
+          ),
           color: Color(0xFF01497C),
+        ),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.fromLTRB(20, 50, 20, 0),
+                child: Text(
+                  'Welcome',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              const Expanded(
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Image(
+                    image: AssetImage("assets/images/newperson3.png"),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 50),
+              Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          // Handle login button tap
+                          print('Login button tapped');
+                        },
+                        child: Container(
+                          height: 50,
+                          width: 120,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFF01497C),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          // Handle sign up button tap
+                          print('Sign Up button tapped');
+                        },
+                        child: Container(
+                          height: 50,
+                          width: 120,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFF01497C),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 100),
+            ],
+          ),
         ),
       ),
     );
